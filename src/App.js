faf193b9ce5cc8217c957cc927882476297832f7
Breyanna Vass <breyannavass@gmail.com>
2019-11-04 16:55:46 -0500

    adding material-ui/core

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 
+import NavBar from "./components/NavBar";
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar />
         <h1>JSON Parsing Application</h1>
       </div>
     );
---
+++ src/components/NavBar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+
+const NavBar = () => {
+  return <AppBar title="Cantina JSON Parse App" />;
+};
+
+export default NavBar;
